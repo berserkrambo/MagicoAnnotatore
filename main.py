bf4d5639a3ef85b5f23341c4d22e0f9d38e223a6
berserkrambo <berserkrambo@gmail.com>
2019-10-10 15:32:28 +0200

    salvataggi di sicurezza

---
+++ main.py
@@ -0,0 +1,160 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QLabel, QProgressBar, QVBoxLayout, \
+    QHBoxLayout, QLabel
+from PyQt5 import QtCore, uic
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+import threading
+import cv2
+import time
+import numpy as np
+
+from utils import VideoLoader, Annotator
+
+
+class App(QWidget):
+    def __init__(self):
+        # super(threading.Thread, self).__init__()
+        super(QWidget, self).__init__()
+        self.vido_loader = None
+        self.initPosClick()
+        self.initUI()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Q:
+            self.deleteLater()
+        elif event.key() == QtCore.Qt.Key_N:
+            img = self.get_next_img()
+            if img is not None:
+                self.load_pix_from_buff(img)
+                self.annotator.save_current()
+            else:
+                self.annotator.save_all()
+
+        event.accept()
+
+    def get_next_img(self):
+        img = self.vido_loader.get_next_img()
+        self.annotator.current_frame += 1
+        return img
+
+    def load_pix(self):
+        img = cv2.cvtColor(cv2.imread('/home/rgasparini/Pictures/giova.png'), cv2.COLOR_BGR2RGB)
+        qscale = 1280 / img.shape[1]
+        img = cv2.resize(img, (0, 0), fx=qscale, fy=qscale)
+        height, width, channel = img.shape
+        bytesPerLine = channel * width
+        qImg = QImage(img.data, width, height, bytesPerLine, QImage.Format_RGB888)
+        self.label.setPixmap(QPixmap.fromImage(qImg))
+        self.label.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+        self.label.update()
+
+    def load_pix_from_buff(self, img):
+        height, width, channel = img.shape
+        bytesPerLine = channel * width
+        qImg = QImage(img.data, width, height, bytesPerLine, QImage.Format_RGB888)
+        self.label.setPixmap(QPixmap.fromImage(qImg))
+        self.label.update()
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        video_file, _ = QFileDialog.getOpenFileName(self, "Selezionare un file video", "",
+                                                    "MP4 video files (*.mp4);;AVI video files (*.avi)", options=options)
+        if video_file is not "":
+            self.vido_loader = VideoLoader(video_file)
+            self.annotator = Annotator(self.vido_loader.tot_frames, video_file.replace(".avi", ".txt").replace(".mp4", ".txt"))
+            while not self.vido_loader.batch_loaded:
+                time.sleep(0.5)
+            self.load_pix_from_buff(self.get_next_img())
+
+    def initPosClick(self):
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+        self.click = 0
+
+    def getPos(self, event):
+        if self.vido_loader is None:
+            return
+        x = event.pos().x()
+        y = event.pos().y()
+        self.click += 1
+        if self.click == 1:
+            self.x1 = x
+            self.y1 = y
+        elif self.click == 2:
+            self.x2 = x
+            self.y2 = y
+            self.click = 0
+            self.x1, self.x2 = min(self.x1, self.x2), max(self.x1, self.x2)
+            self.y1, self.y2 = min(self.y1, self.y2), max(self.y1, self.y2)
+
+            print(f"frame {self.annotator.current_frame}, pos {self.x1},{self.y1} : {self.x2},{self.y2}")
+
+
+    def initUI(self):
+        self.title = 'Magico Annotatore Ferroviario'
+        self.left = 10
+        self.top = 10
+        self.width = 1800
+        self.height = 850
+
+        self.hlay0 = QHBoxLayout(self)
+
+        # load button
+        self.loadButton = QPushButton()
+        self.loadButton.setText("Carica Video")
+        self.loadButton.setToolTip('Clicca per caricare un video')
+        self.loadButton.setMaximumWidth(200)
+        self.loadButton.clicked.connect(self.openFileNameDialog)
+
+        # instructions label
+        self.instrLabel = QLabel()
+        self.instrLabel.move(1350, 120)
+        self.instrLabel.setStyleSheet('color: red')
+        self.instrLabel.setText("Istruzioni:\n "
+                                "1: premere 'carica video'\n"
+                                "per aprire un video ed aspettare\n"
+                                "che venga caricato il primo frame")
+
+        # image label
+        self.label = QLabel()
+        self.label.setMaximumSize(1280, 720)
+        self.label.setMinimumSize(1280, 720)
+        self.load_pix_from_buff(np.zeros((720, 1280, 3), dtype=np.uint8))
+        self.label.mousePressEvent = self.getPos
+
+        # video annotation progress bar
+        self.pbar = QProgressBar()
+        # self.pbar.move(100, 730)
+        self.pbar.setRange(0, 100)
+        self.pbar.setMinimumSize(500, 10)
+        self.pbar.setMaximumWidth(1280)
+        self.pbar.setValue(0)
+
+        # layouts to put all together
+        self.l_vlay0 = QVBoxLayout(self)
+        self.r_vlay0 = QVBoxLayout(self)
+        self.l_vlay0.addWidget(self.label)
+        self.l_vlay0.addWidget(self.pbar)
+        self.r_vlay0.addWidget(self.instrLabel)
+        self.r_vlay0.addWidget(self.loadButton)
+        self.r_vlay0.setAlignment(QtCore.Qt.AlignTop)
+
+        self.hlay0.addLayout(self.l_vlay0)
+        self.r_vlay1 = QVBoxLayout()
+
+        self.hlay0.addLayout(self.r_vlay0)
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setMaximumSize(1600, 800)
+        self.setMinimumSize(1600, 800)
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
---
+++ utils.py
@@ -0,0 +1,117 @@
+import threading
+import cv2
+import time
+
+
+class ClickPos:
+    def __init__(self):
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+        self.click = 0
+
+    def getPos(self):
+        """
+        :return: Pos (x,y) or None if need a second click
+        """
+        self.click += 1
+
+
+class Annotator:
+    def __init__(self, tot_frames, file_name):
+        self.file_name = file_name
+        self.file = open(file_name, 'w')
+        self.total_frames = tot_frames
+        self.current_frame = 0
+        self.frame_objs = {}  # chiave: tupla (frane_number, x,y), valore: classe
+        self.objs = []
+
+    def update_obj(self, pos, val):
+        self.frame_objs[pos] = val
+
+    def remove_obj(self, pos):
+        if pos in self.frame_objs:
+            self.frame_objs.pop(pos)
+
+    def reset(self):
+        self.file = open(self.file_name, 'w')
+        self.current_frame = 0
+        self.frame_objs = {}  # chiave: tupla (frane_number, x,y), valore: classe
+        self.objs = []
+
+    def save_current(self):
+        for obj in self.frame_objs:
+            self.objs.append(obj)
+        self.frame_objs = {}
+
+    def save_all(self):
+        self.save_current()
+        for obj in self.objs:
+            self.file.write(obj)
+            self.file.write('\n')
+        self.objs = []
+
+    def __del__(self):
+        self.save_all()
+        self.file.close()
+
+
+class VideoLoader(threading.Thread):
+    def __init__(self, file, batch_size=300, max_width=1280):
+        super(VideoLoader, self).__init__()
+        self.file = file
+        self.tot_frames = 0
+        self.frame_loaded = 0
+        self.batch_size = batch_size
+        self.current_batch_frame = 0
+        self.max_width = max_width
+        self.batch_imgs = []
+        self.scale_factor = None
+        self.batch_loaded = False
+        self.load()
+
+    def load(self):
+        self.reader = cv2.VideoCapture(self.file)
+        self.tot_frames = int(self.reader.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = int(self.reader.get(cv2.CAP_PROP_FPS))
+        self.video_heigth = int(self.reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.video_width = int(self.reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(
+            f"video {self.file}:\n {self.tot_frames} frames, {time.strftime('%H:%M:%S', time.gmtime(self.tot_frames // self.fps))}, {self.fps} fps, {self.video_width}x{self.video_heigth}")
+
+        if self.max_width is not None:
+            self.scale_factor = self.max_width / self.video_width
+
+        self.get_next_batch()
+
+    def get_next_img(self):
+        if self.current_batch_frame == len(self.batch_imgs) - 1:
+            self.get_next_batch()
+            if not self.batch_loaded:
+                return None
+        img = self.batch_imgs[self.current_batch_frame]
+        self.current_batch_frame += 1
+        return img
+
+    def get_next_batch(self):
+        self.batch_imgs = []
+        self.batch_loaded = False
+        self.current_batch_frame = 0
+        batch_frame_loaded = 0
+
+        ret, frame = self.reader.read()
+        if ret:
+            frame = cv2.resize(frame, (0, 0), fx=self.scale_factor, fy=self.scale_factor)
+            batch_frame_loaded += 1
+            self.frame_loaded += 1
+            self.batch_imgs.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+        while ret and self.frame_loaded < self.tot_frames and batch_frame_loaded < self.batch_size:
+            ret, frame = self.reader.read()
+            if ret:
+                frame = cv2.resize(frame, (0, 0), fx=self.scale_factor, fy=self.scale_factor)
+                batch_frame_loaded += 1
+                self.frame_loaded += 1
+                self.batch_imgs.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        self.batch_loaded = True
